d19aa7862ef4e1029eef230f449ba3c3c62d5bb0
Christopher Brown <nudgechris@gmail.com>
2017-05-09 23:58:48 -0500

    template

--- public/js/calDisplay.js
+++ public/js/calDisplay.js
@@ -21,3 +21,28 @@ function getCals() {
 }
 
 getCals();
+
+var daily = `<div class="day">
+  <div class="day-row">
+    <div class="meal">
+      <h2>Lunch</h2>
+    </div>
+    <div class="meal-items">
+      <div class="item">
+        <h3>Tuna Sammy</h3>
+      </div>
+      <div class="calories">
+        <h3>356</h3>
+      </div>
+    </div>
+    <div class="item">
+      <h3>Chips</h3>
+    </div>
+    <div class="calories">
+      <h3>200</h3>
+    </div>
+  </div>
+  </div>`;
+  var testData = calories[0];
+
+$('#target').append(testData);
