68721c93dce7d6f29edc3ed72aac8acae77699b6
Christopher Brown <nudgechris@gmail.com>
2017-05-10 00:08:52 -0500

    templating

--- public/js/calDisplay.js
+++ public/js/calDisplay.js
@@ -3,6 +3,40 @@ console.log('calDisplay.js loaded');
 
 var calories =[];
 
+
+
+function writeCals() {
+  var testData = calories[0].meals[0].meal;
+  var writeIt = `<h1>${testData}</h1>`;
+  var daily = `
+  <div class="counter-main calorie-display">
+    <div class="day">
+      <div class="day-row">
+        <div class="meal">
+          <h2>${testData}</h2>
+        </div>
+        <div class="meal-items">
+          <div class="item">
+            <h3>Tuna Sammy</h3>
+          </div>
+          <div class="calories">
+            <h3>356</h3>
+          </div>
+        </div>
+        <div class="item">
+          <h3>Chips</h3>
+        </div>
+        <div class="calories">
+          <h3>200</h3>
+        </div>
+        </div>
+      </div>
+      </div>
+    </div>`;
+
+  $('#target').append(daily);
+}
+
 function getCals() {
   // ajax in the calories
   var loadCals = $.ajax({
@@ -14,6 +48,7 @@ function getCals() {
   loadCals.done(function(data) {
     calories.push(data)
     console.log(data)
+    writeCals();
   });
   loadCals.fail(function(jqXHR, testStatus, errorThrown) {
     console.log(errorThrown);
@@ -21,28 +56,3 @@ function getCals() {
 }
 
 getCals();
-
-var daily = `<div class="day">
-  <div class="day-row">
-    <div class="meal">
-      <h2>Lunch</h2>
-    </div>
-    <div class="meal-items">
-      <div class="item">
-        <h3>Tuna Sammy</h3>
-      </div>
-      <div class="calories">
-        <h3>356</h3>
-      </div>
-    </div>
-    <div class="item">
-      <h3>Chips</h3>
-    </div>
-    <div class="calories">
-      <h3>200</h3>
-    </div>
-  </div>
-  </div>`;
-  var testData = calories[0];
-
-$('#target').append(testData);
