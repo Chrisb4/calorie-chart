64647faebbe30ffd4939dda8949f5864fec67ba3
Christopher Brown <nudgechris@gmail.com>
2017-05-24 23:54:31 -0500

    user styles

--- public/js/calDisplay.js
+++ public/js/calDisplay.js
@@ -1,38 +1,22 @@
 console.log('calDisplay.js loaded');
 // Oauth
-
+// User styles 😉
 var calories =[];
 
 
+// data recieved is
+// array of objects, each object is a meal that has 7 keys, we need to look  at
+  // createdAt
+  // meal
+  // food -- this is an array of items
+  // calories -- this is an array of calories from every item
+
 
 function writeCals() {
+
   var testData = calories[0].meals[0].meal;
   var writeIt = `<h1>${testData}</h1>`;
-  var daily = `
-  <div class="counter-main calorie-display">
-    <div class="day">
-      <div class="day-row">
-        <div class="meal">
-          <h2>${testData}</h2>
-        </div>
-        <div class="meal-items">
-          <div class="item">
-            <h3>Tuna Sammy</h3>
-          </div>
-          <div class="calories">
-            <h3>356</h3>
-          </div>
-        </div>
-        <div class="item">
-          <h3>Chips</h3>
-        </div>
-        <div class="calories">
-          <h3>200</h3>
-        </div>
-        </div>
-      </div>
-      </div>
-    </div>`;
+
 
   $('#target').append(daily);
 }
